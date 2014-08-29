a8d1fd415e52169599d0125179422229114b783c
LionC <lionc-ilu@hotmail.de>
2014-08-30 01:50:51 +0200

    Some more writing on the Button class

--- src/ui/elements/Button.java
+++ src/ui/elements/Button.java
@@ -1,18 +1,39 @@
 package ui.elements;
 
-import java.awt.*;
+import model.Actor;
+import ui.Board;
 
-/**
- * Created by LionC on 15.07.2014.
- */
-public class Button extends Box {
+import java.awt.*;
+import java.util.Optional;
+
+public class Button extends Box implements Actor {
     Label label;
+    Runnable action = null;
+    Board board = null;
+
     boolean labelNeedsToBeCentered = true;
 
     public Button(int aX, int aY, int aWidth, int aHeight, String aLabel) {
         super(aX, aY, aWidth, aHeight);
 
-        label = new Label(aLabel);
+        this.label = new Label(aLabel);
+    }
+
+    protected boolean wasClicked() {
+        if(this.board != null) {
+            int mX = this.board.getMouseX(), mY = this.board.getMouseY();
+
+            return board.mouseKeyPressed(1) && mX >= this.getX() && mX <= this.getX()+this.getWidth() && mY >= this.getY() && mY <= this.getY()+this.getHeight();
+        }
+        else {
+            return false;
+        }
+    }
+
+    protected void onClick() {
+        if(this.action != null)
+            if(this.wasClicked())
+                this.action.run();
     }
 
     protected void centerLabel(Graphics2D g) {
@@ -27,9 +48,22 @@ public class Button extends Box {
         this.label.setPosition(posX, posY);
     }
 
+    public void setAction(Runnable aAction) {
+        this.action = aAction;
+    }
+
+    public void setBoard(Board aBoard) {
+        this.board = aBoard;
+    }
+
     @Override
     public void draw(Graphics2D g) {
         super.draw(g);
         this.label.draw(g);
+    }
+
+    @Override
+    public void update() {
+        this.onClick();
     }
 }
