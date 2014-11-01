94f4dcc999780cea02fc5a7157ded2787afd629c
LionC <lionc-ilu@hotmail.de>
2014-11-01 23:12:27 +0100

    Lots of refactoring, still in progress

---
+++ src/model/Drawer.java
@@ -0,0 +1,39 @@
+package model;
+
+import graphics.Drawable;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Drawer {
+    protected List<Drawable> drawables = new ArrayList<>();
+
+    /**
+     * Adds a Drawable to this Manager to be rendered by it
+     *
+     * @param aDrawable Drawable to be added
+     */
+    public void add(Drawable aDrawable) {
+        this.drawables.add(aDrawable);
+    }
+
+    /**
+     * Removes a Drawable from this Manager
+     *
+     * @param aDrawable Drawable to be removed
+     */
+    public void remove(Drawable aDrawable) {
+        this.drawables.remove(aDrawable);
+    }
+
+    /**
+     * Renders all managed Drawables on the given Graphics-Context
+     * @param g Graphics Context to render on
+     */
+    public void render(Graphics2D g) {
+        for(Drawable act : this.drawables) {
+            act.draw(g);
+        }
+    }
+}
---
+++ src/model/Game.java
@@ -0,0 +1,83 @@
+package model;
+
+import ui.Board;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Game {
+    protected int fps = 30;
+    protected int frameLength = 1000 / fps;
+    protected long lastFrame;
+
+    protected List<Board> boards = new ArrayList<>();
+    protected List<Actor> actors = new LinkedList<>();
+
+    public Game() {}
+
+    public Game(int aFps) {
+        this.fps = aFps;
+        this.frameLength = 1000 / aFps;
+    }
+
+    public void addBoard(Board aBoard) {
+        this.boards.add(aBoard);
+    }
+
+    public boolean removeBoard(Board aBoard) {
+        return this.boards.remove(aBoard);
+    }
+
+    public List<Board> getBoards() {
+        //TODO: This is kinda mutable, someGame.getBoards().remove(someBoard) - is this ok?
+        return this.boards;
+    }
+
+    public Board getBoard(int aIndex) {
+        return this.boards.get(aIndex);
+    }
+
+    public Board getBoard() {
+        return this.getBoard(1);
+    }
+
+    public void addActor(Actor aActor) {
+        this.actors.add(aActor);
+    }
+
+    public boolean removeActor(Actor aActor) {
+        return this.actors.remove(aActor);
+    }
+
+    public void setFps(int aFps) {
+        this.fps = aFps;
+        this.frameLength = 1000 / aFps;
+    }
+
+    public void run() {
+        long cur = System.currentTimeMillis();
+        this.lastFrame = cur;
+
+        for(;;) {
+            for(Actor act : this.actors)
+                act.update();
+
+            for(Board act : this.boards)
+                act.update();
+
+            cur = System.currentTimeMillis();
+            this.lastFrame = cur;
+            this.busyWait(this.frameLength - (int)(cur - this.lastFrame));
+        }
+    }
+
+    protected void busyWait(int milis) {
+        if(milis > 0) {
+            long start = System.currentTimeMillis();
+
+            while(System.currentTimeMillis() < (start + milis))
+                Thread.yield();
+        }
+    }
+}
